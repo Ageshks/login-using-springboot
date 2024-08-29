04924203aad4e4abfe623564e754f6918618d870
Agesh k s <71646123+Ageshks@users.noreply.github.com>
2024-08-29 19:31:17 +0530

    Add files via upload

--- src/main/java/agesh/login/test/controller/LoginController.java
+++ src/main/java/agesh/login/test/controller/LoginController.java
@@ -3,20 +3,31 @@ package agesh.login.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+// @Controller
+// public class LoginController {
+
+//     @GetMapping("/login")
+//     public String login() {
+//         return "login";
+//     }
+
+//     @GetMapping("/")
+//     public String home() {
+//         return "home";
+//     }
+//     @GetMapping("/dashboard")
+//     public String dashboard(){
+//         return "dashboard";
+//     }
+// }
+
 @Controller
 public class LoginController {
-
     @GetMapping("/login")
     public String login() {
         return "login";
-    }
-
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
-    @GetMapping("/dashboard")
-    public String dashboard(){
-        return "dashboard";
     }
 }
--- src/main/java/agesh/login/test/controller/UserController.java
+++ src/main/java/agesh/login/test/controller/UserController.java
@@ -1,37 +1,48 @@
 package agesh.login.test.controller;
 
-import agesh.login.test.model.User;
-import agesh.login.test.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+// import agesh.login.test.model.User;
+// import agesh.login.test.service.UserService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+
+// @Controller
+// public class UserController {
+
+//     @Autowired
+//     private UserService userService;
+
+//     @GetMapping("/home")
+//     public String showRegistrationForm() {
+//         return "home";
+//     }
+
+//     @PostMapping("/register")
+//     public String registerUser(User user, Model model) {
+//         userService.registerUser(user);
+//         return "redirect:/login";
+//     }
+
+//     @GetMapping("/login")
+//     public String showLoginForm() {
+//         return "login";
+//     }
+
+//     @GetMapping("/login/dashboard")
+//     public String showDashboard() {
+//         return "dashboard";
+//     }
+// }
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-
-    @Autowired
-    private UserService userService;
-
-    @GetMapping("/home")
-    public String showRegistrationForm() {
-        return "home";
-    }
-
-    @PostMapping("/register")
-    public String registerUser(User user, Model model) {
-        userService.registerUser(user);
-        return "redirect:/login";
-    }
-
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
-    }
-
-    @GetMapping("/login/dashboard")
-    public String showDashboard() {
-        return "dashboard";
     }
 }
